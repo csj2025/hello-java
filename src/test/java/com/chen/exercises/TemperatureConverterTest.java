a908a8787d50a5bb876f1d4b62a35e574e415e90
chen <maoyuluo12@gmail.com>
2025-09-28 17:25:04 +0800

    feat:add temperatureConverterTest

--- src/test/java/com/chen/exercises/TemperatureConverterTest.java
+++ src/test/java/com/chen/exercises/TemperatureConverterTest.java
@@ -1,4 +1,4 @@
-package com.chen;
+package com.chen.exercises;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TemperatureConverterTest {
