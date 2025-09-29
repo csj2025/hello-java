9fe4143770c99653e52d34b032314e3d996bd5ee
chen <maoyuluo12@gmail.com>
2025-09-29 19:28:36 +0800

    feat(sm): add student manager with arraylist

---
+++ src/main/java/com/chen/sm/service/StudentService.java
@@ -0,0 +1,48 @@
+package com.chen.sm.service;
+
+import com.chen.sm.model.Student;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class StudentService {
+    private final List<Student> students = new ArrayList<>();
+    public boolean add(Student s){
+        if(s ==null && s.getId() ==null){
+            return false;
+        }
+        if(this.getById(s.getId())!=null){//判断是否已经存在
+            return false;
+        }
+        return students.add(s);
+    }
+    public boolean remove(String id){
+        Student s = this.getById(id);
+        return s != null && students.remove(s);
+    }
+    public boolean update(String id,String name,Integer age,Double score){
+        Student s = this.getById(id);
+        if(s==null){
+            return false;
+        }
+        if(name !=null){
+            s.setName(name);
+        }
+        if(age !=null){
+            s.setAge(age);
+        }
+        if(score !=null){
+            s.setScore(score);
+        }
+        return true;
+    }
+    public Student getById(String id){
+        for(Iterator<Student> iterator = students.iterator(); iterator.hasNext();){
+            if(iterator.next().getId().equals(id)){
+                return iterator.next();
+            }
+        }
+        return null;
+    }
+}
